b2e8b936f3ab907e786737eddf7dd91ba1e5d7d1
Steve Ndengue <steve.ndengue@gmail.com>
2023-02-28 22:12:54 +0000

    Lab: Instantiation complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object reference = new Object();
+        return reference;
     }
 }
